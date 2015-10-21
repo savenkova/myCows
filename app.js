3a7b97eb69fe34f0c175fd544e38eae665d34aaa
savenkova <doctor.scissor@gmail.com>
2015-10-21 18:39:27 +0500

    форма отправляется, но в базу ничего не добавляет

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ var express = require('express'),
     app = express(),
     jade = require('jade'),
     router = express.Router({}),
-    db = require('db');
+    db = require('db'),
+    multer = require('multer'),
+    upload = multer({ dest: 'uploads/' });
 
 //сохраняем в новоую переменную JSON с коровами
 var getCows = db.get();
 
 app.set('views',  __dirname + '/views');
 app.set('view engine', 'jade');
-
 
 // это мы отправляем на главную
 router.get('/', function (req, res) {
@@ -27,8 +28,8 @@ router.get('/cows', function (req, res) {
     res.render('cows',{"cows" : getCows});
 });
 
-app.post('/cows', function(req, res){
-    var name = req.param('name', null);
+router.post('/cows', upload.array(), function (req, res, next) {
+
 });
 
 // для каждой коровы по idшнику выделяется url, на котором видим вьюшку, заполненную коровой с соответсвующим id
@@ -41,6 +42,9 @@ router.get('/cow/:id', function (req, res) {
 });
 
 app.use(router);
+
+app.use(multer({dest:'./uploads/'}).array('multiInputFileName'));
+app.use(express.static(__dirname + '/public'));
 app.listen(3000, function () {
     console.log('Ready on 3000');
 });
