ddc4858313e985290e63182ecf3d4336afe26a49
savenkova <doctor.scissor@gmail.com>
2015-10-27 17:01:44 +0500

    теперь есть база
    коровы туда добавляются и удаляются

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ var express = require('express'),
     app = express(),
     jade = require('jade'),
     router = express.Router({}),
-    db = require('db'),
     multer = require('multer'),
-    upload = multer({ dest: 'uploads/' });
+    upload = multer({ dest: 'uploads/' }),
+    mongoose = require('mongoose');
 
-//сохраняем в новоую переменную JSON с коровами
-var getCows = db.get();
+mongoose.connect('mongodb://localhost/test');
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+     //console.log("yay!");
+});
+
+var cowSchema = mongoose.Schema({
+    name: String,
+    id: Number
+});
+
+var Cow = mongoose.model('Cow', cowSchema); //cows
 
 app.set('views',  __dirname + '/views');
 app.set('view engine', 'jade');
@@ -17,31 +28,55 @@ app.set('view engine', 'jade');
 // это мы отправляем на главную
 router.get('/', function (req, res) {
     res.render('index',
-            { title: 'Hey',
+        { title: 'Hey',
             message: 'Привет, друг!',
             suggest: 'Хочешь посмотреть наших коровок или добавить своих?',
             link: 'Перейти к коровкам'});
 });
 
-
-// на ./cows отправляем JSON с коровами
+// на ./cows отправляем всех коров
 router.get('/cows', function (req, res) {
-    res.render('cows',{"cows" : getCows});
+    Cow.find(function (err, cows) {
+        if (err) return err;
+        res.render('cows', {
+            "cows": cows
+        });
+    });
 });
 
 //добавляем корову
 router.post('/cows', upload.array(), function (req, res) {
-    db.add(req.body);
-    res.redirect('/cows');
+    var zorka = new Cow({
+        name: req.body.name
+    });
+
+    zorka.save(function(err, zorka) {
+        if (err) return console.error(err);
+        res.redirect('/cows');
+    });
+
 });
 
 // для каждой коровы по idшнику выделяется url, на котором видим вьюшку, заполненную коровой с соответсвующим id
 router.get('/cow/:id', function (req, res) {
-    for (var cow in getCows) {
-        if (getCows[cow].id == req.params.id){
-            res.render('cow', {"cow": getCows[cow]});
-        };
-   }
+    Cow.find({ _id: req.params.id }, function (err, cow) {
+        if (err) return err;
+        res.render('cow', {
+            "cow": cow[0]
+        });
+    });
+});
+
+// удаляем
+router.get('/cow/:id/delete', function (req, res) {
+    Cow.remove({ _id: req.params.id }, function (err) {
+        if (err) return err;
+        res.redirect('/cows');
+        Cow.find(function (err, cows) {
+            if (err) return err;
+            //console.log(cows)
+        });
+    });
 });
 
 app.use(router);
